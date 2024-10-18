cf9e21012d134ef8fdfedf1bb2b19f188f2ba443
M Hassan Tariq <mhtariq29@gmail.com>
2024-10-19 01:22:17 +0500

    feat: simple db setup

---
+++ src/modules/orgs/dto/orgs.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateOrg {
+  @ApiProperty({ example: 'CyFy', required: true })
+  name: string;
+}
+
+export class FetchOrg extends CreateOrg {
+  @ApiProperty({
+    example: '677e7abf-a71e-4a3e-84b2-5baf2b1d9651',
+    required: true,
+  })
+  id: string;
+}
