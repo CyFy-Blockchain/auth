12baf952a5451699d373d734e42acef06e85e37b
M Hassan Tariq <mhtariq29@gmail.com>
2024-12-07 16:44:08 +0500

    docs: update swagger config

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -9,8 +9,5 @@ export const swaggerConfig = new DocumentBuilder()
   .setDescription('This documentation is for Auth documentation')
   .setVersion('1.0.0')
   .addServer('http://localhost:4000', 'Local environment')
-  .addApiKey(
-    { type: 'apiKey', name: 'token', in: 'header' },
-    'User token for authentication',
-  )
+  .addServer('https://auth-5m69.onrender.com/', 'Dev environment')
   .build();
